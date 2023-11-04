20f9c3a7d95e80dd7cf43d7a105f00b466d4813a
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-11-04 09:12:23 +0000

    Add files via upload

---
+++ nestat.if.py.py
@@ -0,0 +1,8 @@
+x = input ("enter your number ? ")
+y = int(x)
+print ("the number in ",y)
+if y > 2:
+    print("y is bigger than 2")
+    if y < 100:
+        print ("y is smaller than 100")
+print ("all done")
